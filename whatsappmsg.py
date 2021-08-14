f9ef473019c0496658c4bc31339f256094df8424
Ajith Kumar.P <87603345+AjithAju19@users.noreply.github.com>
2021-08-14 19:39:44 +0530

    Update whatsappmsg.py

--- whatsappmsg.py
+++ whatsappmsg.py
@@ -1,2 +1,7 @@
 import pywhatkit
-pywhatkit.sendwhatmsg('+919500976661','?',14,45)
+pywhatkit.sendwhatmsg('+91__________','?',14,45)
+
+#Type phone number in the given blanck.
+#Type the message to be sent,in place of ?
+#Here 14 and 45 represents time in minutes and seconds,change it accordingly and specify the time that you need to send the message.
+
